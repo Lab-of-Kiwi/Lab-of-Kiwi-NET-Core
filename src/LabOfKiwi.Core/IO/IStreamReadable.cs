8c2e3c60d40c003a2db2734051fa498cac294416
Steven Johnson <sjohnson0684@gmail.com>
2022-05-26 17:31:24 -0400

    Interfaces for objects to implement that allows them to be either written to stream or read from stream.

---
+++ src/LabOfKiwi.Core/IO/IStreamReadable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LabOfKiwi.IO
+{
+    /// <summary>
+    /// Represents an object that can be populated by reading a <see cref="Stream"/>.
+    /// </summary>
+    public interface IStreamReadable
+    {
+        /// <summary>
+        /// Reads the provided <see cref="Stream"/> and populates this instance with the data read.
+        /// </summary>
+        /// 
+        /// <param name="stream">The <see cref="Stream"/> to read.</param>
+        /// 
+        /// <exception cref="ArgumentNullException"><paramref name="stream"/> is <c>null</c>.</exception>
+        /// <exception cref="IOException">An error occurs when reading <paramref name="stream"/>.</exception>
+        /// <exception cref="EndOfStreamException">The end of <paramref name="stream"/> has been reached.</exception>
+        void Read(Stream stream);
+
+        /// <summary>
+        /// Asynchrously reads the provided <see cref="Stream"/> and populates this instance with the data read.
+        /// </summary>
+        /// 
+        /// <param name="stream">The <see cref="Stream"/> to read.</param>
+        /// <param name="cancellationToken">The token to monitor for cancellation requests.</param>
+        /// <returns>A task that represents the asynchronous write operation.</returns>
+        /// 
+        /// <exception cref="ArgumentNullException"><paramref name="stream"/> is <c>null</c>.</exception>
+        /// <exception cref="IOException">An error occurs when reading <paramref name="stream"/>.</exception>
+        /// <exception cref="EndOfStreamException">The end of <paramref name="stream"/> has been reached.</exception>
+        /// <exception cref="StreamReadTaskCanceledException">Task is cancelled before all bytes are read.</exception>
+        ValueTask ReadAsync(Stream stream, CancellationToken cancellationToken = default);
+    }
+}
