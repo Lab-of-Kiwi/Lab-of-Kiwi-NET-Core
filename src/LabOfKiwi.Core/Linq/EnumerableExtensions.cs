cc41a3e6c016a8e31d33aaa65fcbb372763d0eef
Steven Johnson <sjohnson0684@gmail.com>
2022-07-10 15:45:08 -0400

    Documentation for EnumerableExtensions.

--- src/LabOfKiwi.Core/Linq/EnumerableExtensions.cs
+++ src/LabOfKiwi.Core/Linq/EnumerableExtensions.cs
@@ -1,13 +1,31 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 
 namespace LabOfKiwi.Linq;
 
+/// <summary>
+/// Linq extensions.
+/// </summary>
 public static class EnumerableExtensions
 {
-    public static IReadOnlyList<TSource> ToReadOnlyList<TSource>(this IEnumerable<TSource> source)
+    /// <summary>
+    /// Returns a read-only <see cref="ReadOnlyCollection{T}"/> wrapper for the provided collection.
+    /// </summary>
+    /// 
+    /// <typeparam name="TSource">The type of elements in the collection.</typeparam>
+    /// <param name="source">The collection to be turned into a <see cref="ReadOnlyCollection{T}"/>.</param>
+    /// <returns>An object that acts as a read-only wrapper around the provided collection.</returns>
+    /// 
+    /// <exception cref="ArgumentNullException"><paramref name="source"/> is <c>null</c>.</exception>
+    public static ReadOnlyCollection<TSource> ToReadOnlyList<TSource>(this IEnumerable<TSource> source)
     {
+        if (source == null)
+        {
+            throw new ArgumentNullException(nameof(source));
+        }
+
         if (source is ReadOnlyCollection<TSource> roColl)
         {
             return roColl;
