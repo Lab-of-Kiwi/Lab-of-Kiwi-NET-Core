6c20052ee25ca484237338e1027ca537278f5735
Steven Johnson <sjohnson0684@gmail.com>
2022-07-26 21:35:33 -0400

    Improve on Enum cache methods.

--- src/LabOfKiwi.Core/Enums.cs
+++ src/LabOfKiwi.Core/Enums.cs
@@ -5,19 +5,21 @@ namespace LabOfKiwi;
 
 public static class Enums
 {
-    public static Dictionary<TEnum, string> CreateCache<TEnum>() where TEnum : struct, Enum
+    public static Dictionary<TEnum, string> CreateDefaultCache<TEnum>() where TEnum : struct, Enum
     {
-        return CreateCache<TEnum>(e => e.ToString());
+        return CreateCache<TEnum, string>(e => e.ToString());
     }
 
-    public static Dictionary<TEnum, string> CreateCache<TEnum>(Func<TEnum, string> callback) where TEnum : struct, Enum
+    public static Dictionary<TEnum, TOutput> CreateCache<TEnum, TOutput>(Func<TEnum, TOutput> callback)
+        where TEnum : struct, Enum
+        where TOutput : notnull
     {
         if (callback == null)
         {
             throw new ArgumentNullException(nameof(callback));
         }
 
-        var cache = new Dictionary<TEnum, string>();
+        var cache = new Dictionary<TEnum, TOutput>();
 
         foreach (var value in Enum.GetValues<TEnum>())
         {
