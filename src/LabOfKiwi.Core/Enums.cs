2a4ca44489389a56ac9bed5965801165065af70c
Steven Johnson <sjohnson0684@gmail.com>
2022-07-26 21:31:09 -0400

    Implement common method for caching Enum values.

---
+++ src/LabOfKiwi.Core/Enums.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LabOfKiwi;
+
+public static class Enums
+{
+    public static Dictionary<TEnum, string> CreateCache<TEnum>() where TEnum : struct, Enum
+    {
+        return CreateCache<TEnum>(e => e.ToString());
+    }
+
+    public static Dictionary<TEnum, string> CreateCache<TEnum>(Func<TEnum, string> callback) where TEnum : struct, Enum
+    {
+        if (callback == null)
+        {
+            throw new ArgumentNullException(nameof(callback));
+        }
+
+        var cache = new Dictionary<TEnum, string>();
+
+        foreach (var value in Enum.GetValues<TEnum>())
+        {
+            cache.Add(value, callback(value));
+        }
+
+        return cache;
+    }
+}
