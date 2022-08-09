e0248b6dbbc471791b695bca4f47fbe5861e6fac
Steven Johnson <sjohnson0684@gmail.com>
2022-08-09 12:42:37 -0400

    More Linq extensions.

--- src/LabOfKiwi.Core/Linq/EnumerableExtensions.cs
+++ src/LabOfKiwi.Core/Linq/EnumerableExtensions.cs
@@ -45,6 +45,208 @@ public static class EnumerableExtensions
         return new EnumerableList<TSource>(source);
     }
 
+    /// <summary>
+    /// Combines the two provided <see cref="IReadOnlyList{T}"/> instances into a single <see cref="IReadOnlyList{T}"/>.
+    /// </summary>
+    /// 
+    /// <typeparam name="TSource">The type of elements in the list.</typeparam>
+    /// <param name="source">The first <see cref="IReadOnlyList{T}"/>.</param>
+    /// <param name="other">The second <see cref="IReadOnlyList{T}"/>.</param>
+    /// <returns>
+    ///     A <see cref="IReadOnlyList{T}"/> that combines <paramref name="other"/> and <paramref name="source"/>.
+    /// </returns>
+    /// 
+    /// <exception cref="ArgumentNullException"><paramref name="source"/> is <c>null</c>.</exception>
+    /// <exception cref="ArgumentException">
+    ///     The resulting <see cref="IReadOnlyList{T}"/> would have an element count that exceeds
+    ///     <see cref="int.MaxValue"/>.
+    /// </exception>
+    public static IReadOnlyList<TSource> Combine<TSource>(this IReadOnlyList<TSource> source, IReadOnlyList<TSource>? other)
+    {
+        if (source == null)
+        {
+            throw new ArgumentNullException(nameof(source));
+        }
+
+        if (other == null || other.Count == 0)
+        {
+            return source;
+        }
+
+        if (source.Count == 0)
+        {
+            return other;
+        }
+
+        if (source is CombinedList<TSource> multiList)
+        {
+            return multiList.AddList(other);
+        }
+
+        return new CombinedList<TSource>(new IReadOnlyList<TSource>[] { source, other });
+    }
+
+    /// <summary>
+    /// Selects a random element from the provided collection.
+    /// </summary>
+    /// 
+    /// <typeparam name="TSource">The type of elements in the list.</typeparam>
+    /// <param name="source">The collection that a random element will be selected from.</param>
+    /// <returns>A random element of type <typeparamref name="TSource"/>.</returns>
+    /// 
+    /// <exception cref="ArgumentNullException"><paramref name="source"/> is <c>null</c>.</exception>
+    /// <exception cref="InvalidOperationException">The source sequence is empty.</exception>
+    public static TSource Random<TSource>(this IEnumerable<TSource> source)
+    {
+        if (source == null)
+        {
+            throw new ArgumentNullException(nameof(source));
+        }
+
+        int count = source.Count();
+
+        if (count == 0)
+        {
+            throw new InvalidOperationException("Sequence contains no elements.");
+        }
+
+        var random = new Random();
+        int index = random.Next(0, count);
+
+        try
+        {
+            return source.ElementAt(index);
+        }
+        catch
+        {
+            // In case collection is modified between count check.
+            throw new InvalidOperationException("Sequence contains no elements.");
+        }
+    }
+
+    /// <summary>
+    /// Selects a random element from the provided collection.
+    /// </summary>
+    /// 
+    /// <typeparam name="TSource">The type of elements in the list.</typeparam>
+    /// <param name="source">The collection that a random element will be selected from.</param>
+    /// <returns>
+    ///     A random element of type <typeparamref name="TSource"/> or the default value of <paramref name="source"/> is
+    ///     the collection is empty.
+    /// </returns>
+    /// 
+    /// <exception cref="ArgumentNullException"><paramref name="source"/> is <c>null</c>.</exception>
+    public static TSource? RandomOrDefault<TSource>(this IEnumerable<TSource> source)
+    {
+        if (source == null)
+        {
+            throw new ArgumentNullException(nameof(source));
+        }
+
+        int count = source.Count();
+
+        if (count == 0)
+        {
+            return default;
+        }
+
+        var random = new Random();
+        int index = random.Next(0, count);
+
+        try
+        {
+            return source.ElementAt(index);
+        }
+        catch
+        {
+            // In case collection is modified between count check.
+            return default;
+        }
+    }
+
+    // Internal wrapper for combining lists.
+    private readonly struct CombinedList<T> : IReadOnlyList<T>
+    {
+        private readonly IReadOnlyList<T>[] _lists;
+        private readonly int _count;
+
+        public CombinedList(IReadOnlyList<T>[] lists)
+        {
+            long lCount = 0;
+
+            for (int i = 0; i < lists.Length; i++)
+            {
+                var list = lists[i];
+
+                if (list != null)
+                {
+                    lCount += list.Count;
+
+                    if (lCount > int.MaxValue)
+                    {
+                        throw new ArgumentException("Resulting list contains too many elements");
+                    }
+                }
+            }
+
+            _lists = lists;
+            _count = (int)lCount;
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(index));
+                }
+
+                for (int i = 0; i < _lists.Length; i++)
+                {
+                    var list = _lists[i];
+
+                    if (index < list.Count)
+                    {
+                        return list[index];
+                    }
+
+                    index -= list.Count;
+                }
+
+                throw new ArgumentOutOfRangeException(nameof(index));
+            }
+        }
+
+        public int Count => _count;
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < _lists.Length; i++)
+            {
+                var list = _lists[i];
+
+                for (int j = 0; j < list.Count; j++)
+                {
+                    yield return list[j];
+                }
+            }
+        }
+
+        public CombinedList<T> AddList(IReadOnlyList<T> list)
+        {
+            IReadOnlyList<T>[] array = new IReadOnlyList<T>[list.Count + 1];
+            Array.Copy(_lists, array, array.Length);
+            array[list.Count] = list;
+            return new CombinedList<T>(array);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
     // Internal wraper for enumerables to be lists.
     private readonly struct EnumerableList<TSource> : IReadOnlyList<TSource>
     {
