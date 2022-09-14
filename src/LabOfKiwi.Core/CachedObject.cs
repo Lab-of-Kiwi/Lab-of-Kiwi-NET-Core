ea4f5ed5e1b397a41124a806c7525fd65d766ff9
Steven Johnson <sjohnson0684@gmail.com>
2022-09-14 15:29:45 -0400

    Updates to cached object to allow limiting access count.

--- src/LabOfKiwi.Core/CachedObject.cs
+++ src/LabOfKiwi.Core/CachedObject.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace LabOfKiwi;
 
 /// <summary>
-/// Provides methodology to cache a value for a specified time, and generates the value again after said time.
+/// Provides methodology to cache a value for a specified time and/or a number of accesses, and generates the value
+/// again if either timedout or the maximum number of accesses has been reached.
 /// </summary>
 /// 
 /// <typeparam name="T">The type of value to cache.</typeparam>
@@ -18,17 +20,57 @@ public sealed class CachedObject<T>
     // The allowed time to pass before timing out.
     private readonly TimeSpan _timeout;
 
+    // The maximum number of accesses to cached value before resetting.
+    private readonly long _maxCachedAccessCount;
+
     // The current value, if set.
     private T _value = default!;
 
     // Flag determining if value is set.
-    private volatile bool _isSet = false;
+    private bool _isSet = false;
 
     // Time in which value was set.
     private DateTime _setAt;
 
     // Exception thrown during last call to supplier.
-    private volatile Exception? _exception = null;
+    private Exception? _exception = null;
+
+    // The number of times the value has been accessed.
+    private long _cachedAccessCount = -1L;
+
+    /// <summary>
+    /// Creates a new instance of <see cref="CachedObject{T}"/>.
+    /// </summary>
+    /// 
+    /// <param name="timeout">The amount of time to keep the value when generated.</param>
+    /// <param name="maxCachedAccessCount">
+    ///     The maximum number of access attempts allowed after value has been created.
+    /// </param>
+    /// <param name="supplier">The function that supplies the value.</param>
+    /// 
+    /// <exception cref="ArgumentNullException"><paramref name="supplier"/> is <c>null</c>.</exception>
+    /// <exception cref="ArgumentOutOfRangeException">
+    ///     <paramref name="timeout"/> is negative.
+    ///     -or -
+    ///     <paramref name="maxCachedAccessCount"/> is negative.
+    /// </exception>
+    public CachedObject(TimeSpan timeout, long maxCachedAccessCount, Func<T> supplier)
+    {
+        _supplier = supplier ?? throw new ArgumentNullException(nameof(supplier));
+
+        if (timeout < TimeSpan.Zero)
+        {
+            throw new ArgumentOutOfRangeException(nameof(timeout));
+        }
+
+        if (maxCachedAccessCount < 0L)
+        {
+            throw new ArgumentOutOfRangeException(nameof(maxCachedAccessCount));
+        }
+
+        _timeout = timeout;
+        _maxCachedAccessCount = maxCachedAccessCount;
+    }
 
     /// <summary>
     /// Creates a new instance of <see cref="CachedObject{T}"/>.
@@ -39,16 +81,75 @@ public sealed class CachedObject<T>
     /// 
     /// <exception cref="ArgumentNullException"><paramref name="supplier"/> is <c>null</c>.</exception>
     /// <exception cref="ArgumentOutOfRangeException"><paramref name="timeout"/> is negative.</exception>
-    public CachedObject(TimeSpan timeout, Func<T> supplier)
+    public CachedObject(TimeSpan timeout, Func<T> supplier) : this(timeout, long.MaxValue, supplier)
     {
-        _supplier = supplier ?? throw new ArgumentNullException(nameof(supplier));
+    }
 
-        if (timeout < TimeSpan.Zero)
+    /// <summary>
+    /// Creates a new instance of <see cref="CachedObject{T}"/>.
+    /// </summary>
+    /// 
+    /// <param name="maxCachedAccessCount">
+    ///     The maximum number of access attempts allowed after value has been created.
+    /// </param>
+    /// <param name="supplier">The function that supplies the value.</param>
+    /// 
+    /// <exception cref="ArgumentNullException"><paramref name="supplier"/> is <c>null</c>.</exception>
+    /// <exception cref="ArgumentOutOfRangeException"><paramref name="maxCachedAccessCount"/> is negative.</exception>
+    public CachedObject(long maxCachedAccessCount, Func<T> supplier) : this(TimeSpan.MaxValue, maxCachedAccessCount, supplier)
+    {
+    }
+
+    /// <summary>
+    /// Indicates whether a value has been created.
+    /// </summary>
+    public bool IsValueCreated
+    {
+        get
         {
-            throw new ArgumentOutOfRangeException(nameof(timeout));
+            lock (_syncLock)
+            {
+                return _isSet;
+            }
         }
+    }
 
-        _timeout = timeout;
+    /// <summary>
+    /// Indicates whether a value has been attempted to be created but faulted.
+    /// </summary>
+    public bool IsValueFaulted
+    {
+        get
+        {
+            lock (_syncLock)
+            {
+                return _isSet && _exception != null;
+            }
+        }
+    }
+
+    /// <summary>
+    /// The total number of times a cached value can be accessed before being generated again.
+    /// </summary>
+    public long MaxCachedAccessCount => _maxCachedAccessCount;
+
+    /// <summary>
+    /// The remaining number of times the current value can be accessed before being generated again.
+    /// </summary>
+    public long RemainingCachedAccessCount
+    {
+        get
+        {
+            lock (_syncLock)
+            {
+                if (_isSet && _cachedAccessCount < _maxCachedAccessCount && DateTime.Now - _setAt < _timeout)
+                {
+                    return _maxCachedAccessCount - _cachedAccessCount;
+                }
+
+                return 0L;
+            }
+        }
     }
 
     /// <summary>
@@ -63,13 +164,11 @@ public sealed class CachedObject<T>
     {
         get
         {
-            var now = DateTime.Now;
-
             lock (_syncLock)
             {
-                if (_isSet)
+                if (_isSet && _cachedAccessCount < _maxCachedAccessCount)
                 {
-                    var elapsedTime = now - _setAt;
+                    var elapsedTime = DateTime.Now - _setAt;
 
                     if (elapsedTime < _timeout)
                     {
@@ -85,16 +184,17 @@ public sealed class CachedObject<T>
     /// <summary>
     /// Gets the value.
     /// </summary>
+    [DebuggerBrowsable(DebuggerBrowsableState.Never)]
     public T Value
     {
         get
         {
-            var now = DateTime.Now;
-
             lock (_syncLock)
             {
-                if (_isSet && now - _setAt < _timeout)
+                if (_isSet && _cachedAccessCount < _maxCachedAccessCount && DateTime.Now - _setAt < _timeout)
                 {
+                    _cachedAccessCount++;
+
                     if (_exception != null)
                     {
                         throw _exception;
@@ -117,6 +217,7 @@ public sealed class CachedObject<T>
                 {
                     _setAt = DateTime.Now;
                     _isSet = true;
+                    _cachedAccessCount = 0L;
                 }
 
                 return _value;
@@ -133,6 +234,7 @@ public sealed class CachedObject<T>
         {
             _isSet = false;
             _setAt = default;
+            _cachedAccessCount = -1L;
             _exception = null;
             _value = default!;
         }
