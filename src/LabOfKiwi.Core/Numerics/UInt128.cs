919c77a6f7b28e1ca1979b36047b253df3386532
Steven Johnson <sjohnson0684@gmail.com>
2022-09-14 15:35:16 -0400

    Initial work on UInt128 and UInt24 types.

---
+++ src/LabOfKiwi.Core/Numerics/UInt128.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabOfKiwi.Numerics;
+
+public readonly struct UInt128
+{
+    /// <summary>
+    /// <c>340,282,366,920,938,463,463,374,607,431,768,211,455</c>
+    /// </summary>
+    public static readonly UInt128 MaxValue = new(ulong.MaxValue, ulong.MaxValue);
+    public static readonly UInt128 MinValue = new(0UL, 0UL);
+
+    private readonly ulong _lo;
+    private readonly ulong _hi;
+    
+    private UInt128(ulong lo, ulong hi)
+    {
+        _lo = lo;
+        _hi = hi;
+    }
+
+    private BigInteger BigIntegerValue => new(ToByteArray(), isUnsigned: true, isBigEndian: !BitConverter.IsLittleEndian);
+
+    private byte[] ToByteArray()
+    {
+        byte[] arr = new byte[2 * sizeof(ulong)];
+
+        using (var ms = new MemoryStream(arr))
+        {
+            if (BitConverter.IsLittleEndian)
+            {
+                ms.Write(BitConverter.GetBytes(_lo));
+                ms.Write(BitConverter.GetBytes(_hi));
+            }
+            else
+            {
+                ms.Write(BitConverter.GetBytes(_hi));
+                ms.Write(BitConverter.GetBytes(_lo));
+            }
+        }
+
+        return arr;
+    }
+
+    public override string ToString()
+    {
+        if (_hi == 0UL)
+        {
+            return _lo.ToString();
+        }
+
+        return BigIntegerValue.ToString();
+    }
+
+    public string ToString(int toBase)
+    {
+        if (toBase == 2)
+        {
+            string lo = Convert.ToString((long)_lo, 1).PadLeft(64, '0');
+            string hi = Convert.ToString((long)_hi, 1);
+            return (hi + lo).TrimStart('0');
+        }
+
+        if (toBase == 10)
+        {
+            return ToString();
+        }
+
+        if (toBase == 16)
+        {
+            string lo = Convert.ToString((long)_lo, 16).PadLeft(16, '0');
+            string hi = Convert.ToString((long)_hi, 16);
+            return (hi + lo).TrimStart('0');
+        }
+
+        throw new ArgumentException("Unsupported base: " + toBase);
+    }
+
+    #region Implicit Operators
+    public static implicit operator UInt128 (byte v)   => new(v, 0UL);
+    public static implicit operator UInt128 (ushort v) => new(v, 0UL);
+    public static implicit operator UInt128 (uint v)   => new(v, 0UL);
+    public static implicit operator UInt128 (ulong v)  => new(v, 0UL);
+
+    public static implicit operator BigInteger (UInt128 v) => v.BigIntegerValue;
+    #endregion
+
+    #region Explicit Operators
+    public static explicit operator sbyte (UInt128 v) => (sbyte)v._lo;
+    public static explicit operator short (UInt128 v) => (short)v._lo;
+    public static explicit operator int   (UInt128 v) => (int)v._lo;
+    public static explicit operator long  (UInt128 v) => (long)v._lo;
+
+    public static explicit operator byte   (UInt128 v) => (byte)v._lo;
+    public static explicit operator ushort (UInt128 v) => (ushort)v._lo;
+    public static explicit operator uint   (UInt128 v) => (uint)v._lo;
+    public static explicit operator ulong  (UInt128 v) => v._lo;
+
+    public static explicit operator double (UInt128 v) => (double)v.BigIntegerValue;
+    public static explicit operator float  (UInt128 v) => (float)v.BigIntegerValue;
+
+    public static explicit operator UInt128(sbyte v)
+    {
+        ulong hi = v < 0 ? ulong.MaxValue : 0UL;
+        ulong lo = (ulong)v;
+        return new UInt128(lo, hi);
+    }
+
+    public static explicit operator UInt128(short v)
+    {
+        ulong hi = v < 0 ? ulong.MaxValue : 0UL;
+        ulong lo = (ulong)v;
+        return new UInt128(lo, hi);
+    }
+
+    public static explicit operator UInt128(int v)
+    {
+        ulong hi = v < 0 ? ulong.MaxValue : 0UL;
+        ulong lo = (ulong)v;
+        return new UInt128(lo, hi);
+    }
+
+    public static explicit operator UInt128(long v)
+    {
+        ulong hi = v < 0 ? ulong.MaxValue : 0UL;
+        ulong lo = (ulong)v;
+        return new UInt128(lo, hi);
+    }
+
+    public static explicit operator UInt128(BigInteger v)
+    {
+        byte[] arr = v.ToByteArray(isUnsigned: true, isBigEndian: !BitConverter.IsLittleEndian);
+
+        // Only lo bits are needed
+        if (arr.Length < 65)
+        {
+            if (arr.Length != 64)
+            {
+                if (BitConverter.IsLittleEndian)
+                {
+                    arr = arr.PadRight(64, (byte)0);
+                }
+                else
+                {
+                    arr = arr.PadLeft(64, (byte)0);
+                }
+            }
+
+            return new UInt128(BitConverter.ToUInt64(arr), 0UL);
+        }
+
+        if (arr.Length < 128)
+        {
+            if (BitConverter.IsLittleEndian)
+            {
+                arr = arr.PadRight(128, (byte)0);
+            }
+            else
+            {
+                arr = arr.PadLeft(128, (byte)0);
+            }
+        }
+
+        ulong lo, hi;
+
+        if (BitConverter.IsLittleEndian)
+        {
+            lo = BitConverter.ToUInt64(arr);
+            hi = BitConverter.ToUInt64(arr, 64);
+        }
+        else
+        {
+            hi = BitConverter.ToUInt64(arr);
+            lo = BitConverter.ToUInt64(arr, 64);
+        }
+
+        return new UInt128(lo, hi);
+    }
+    #endregion
+
+    #region Arithmetic Operators
+    public static UInt128 operator +(UInt128 left, UInt128 right)
+    {
+        return (UInt128)(left.BigIntegerValue + right.BigIntegerValue);
+    }
+
+    public static UInt128 operator -(UInt128 left, UInt128 right)
+    {
+        return (UInt128)(left.BigIntegerValue - right.BigIntegerValue);
+    }
+    #endregion
+}
---
+++ src/LabOfKiwi.Core/Numerics/UInt24.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabOfKiwi.Numerics;
+
+[DebuggerDisplay("{DebugDisplay()}")]
+public readonly struct UInt24 : IComparable, IComparable<UInt24>, IEquatable<UInt24>
+{
+    /// <summary>
+    /// <c>16777215</c>
+    /// </summary>
+    public static readonly UInt24 MaxValue = new(ushort.MaxValue, byte.MaxValue);
+    public static readonly UInt24 MinValue = new(0, 0);
+
+    public static readonly UInt24 One = new(1, 0);
+
+    private readonly ushort _lo;
+    private readonly byte _hi;
+
+    private UInt24(ushort lo, byte hi)
+    {
+        _lo = lo;
+        _hi = hi;
+    }
+
+    #region Public Members
+    public int CompareTo(object? obj)
+    {
+        if (obj == null)
+        {
+            return 1;
+        }
+
+        if (obj is UInt24 other)
+        {
+            return CompareTo(other);
+        }
+
+        throw new ArgumentException("Value must be of type UInt24.");
+    }
+
+    public int CompareTo(UInt24 other)
+    {
+        int c = _hi.CompareTo(other._hi);
+
+        if (c != 0)
+        {
+            return c;
+        }
+
+        return _lo.CompareTo(other._lo);
+    }
+
+    public override bool Equals([NotNullWhen(true)] object? obj)
+    {
+        return obj is UInt24 other && Equals(other);
+    }
+
+    public bool Equals(UInt24 other)
+    {
+        return _lo == other._lo && _hi == other._hi;
+    }
+
+    public override int GetHashCode()
+    {
+        return (int)this;
+    }
+
+    public int LeadingZeroCount()
+    {
+        return Math.Min(24, BitOperations.LeadingZeroCount((uint)this));
+    }
+
+    public int Log2()
+    {
+        return BitOperations.Log2((uint)this);
+    }
+
+    public int PopCount()
+    {
+        return Math.Min(24, BitOperations.PopCount((uint)this));
+    }
+
+    public UInt24 RotateLeft(int offset)
+    {
+        return (this << offset) | (this >> (24 - offset));
+    }
+
+    public UInt24 RotateRight(int offset)
+    {
+        return (this >> offset) | (this << (24 - offset));
+    }
+
+    public override string ToString()
+    {
+        return ((uint)this).ToString();
+    }
+
+    public int TrailingZeroCount()
+    {
+        return Math.Min(24, BitOperations.TrailingZeroCount((uint)this));
+    }
+
+    public string ToString(int toBase)
+    {
+        return Convert.ToString((uint)this, toBase);
+    }
+    #endregion
+
+    #region Non-Public Operators
+    private string DebugDisplay() => "0b" + Convert.ToString(this, 2).PadLeft(24, '0');
+    #endregion
+
+    #region Implicit Operators
+    // Unsigned => UInt24
+    public static implicit operator UInt24(byte v)   => new(v, 0);
+    public static implicit operator UInt24(ushort v) => new(v, 0);
+
+    // UInt24 => Unsigned
+    public static implicit operator uint  (UInt24 v) => ((uint)v._hi << 16) | v._lo;
+    public static implicit operator ulong (UInt24 v) => (uint)v;
+
+    // UInt24 => Signed
+    public static implicit operator int  (UInt24 v) => (int)(uint)v;
+    public static implicit operator long (UInt24 v) => (uint)v;
+
+    // UInt24 => Floating Point
+    public static implicit operator float  (UInt24 v) => (uint)v;
+    public static implicit operator double (UInt24 v) => (uint)v;
+
+    // Misc.
+    public static implicit operator UInt24(char v) => new(v, 0);
+    #endregion
+
+    #region Explicit Operators
+    // Unsigned => UInt24
+    public static explicit operator UInt24(uint v)  => new((ushort)v, (byte)(v >> 16));
+    public static explicit operator UInt24(ulong v) => new((ushort)v, (byte)(v >> 16));
+
+    // Signed => UInt24
+    public static explicit operator UInt24(sbyte v) => new((byte)v, 0);
+    public static explicit operator UInt24(short v) => new((ushort)v, 0);
+    public static explicit operator UInt24(int v)   => new((ushort)v, (byte)(v >> 16));
+    public static explicit operator UInt24(long v)  => new((ushort)v, (byte)(v >> 16));
+
+    // Floating Point => UInt24
+    public static explicit operator UInt24(float v)  => (UInt24)(uint)v;
+    public static explicit operator UInt24(double v) => (UInt24)(uint)v;
+
+    // UInt24 => Unsigned
+    public static explicit operator byte   (UInt24 v) => (byte)v._lo;
+    public static explicit operator ushort (UInt24 v) => v._lo;
+
+    // UInt24 => Signed
+    public static explicit operator sbyte (UInt24 v) => (sbyte)v._lo;
+    public static explicit operator short (UInt24 v) => (short)v._lo;
+    #endregion
+
+    #region Comparison Operators
+    // Equality
+    public static bool operator ==(UInt24 left, UInt24 right)
+    {
+        return left.Equals(right);
+    }
+
+    // Inequality
+    public static bool operator !=(UInt24 left, UInt24 right)
+    {
+        return !(left == right);
+    }
+
+    // Less Than
+    public static bool operator <(UInt24 left, UInt24 right)
+    {
+        return left.CompareTo(right) < 0;
+    }
+
+    // Less Than or Equal
+    public static bool operator <=(UInt24 left, UInt24 right)
+    {
+        return left.CompareTo(right) <= 0;
+    }
+
+    // Greater Than
+    public static bool operator >(UInt24 left, UInt24 right)
+    {
+        return left.CompareTo(right) > 0;
+    }
+
+    // Greater Than or Equal
+    public static bool operator >=(UInt24 left, UInt24 right)
+    {
+        return left.CompareTo(right) >= 0;
+    }
+    #endregion
+
+    #region Arithmetic Operators
+    // Addition
+    public static UInt24 operator +(UInt24 left, UInt24 right)
+    {
+        return (UInt24)((uint)left + (uint)right);
+    }
+
+    // Subtraction
+    public static UInt24 operator -(UInt24 left, UInt24 right)
+    {
+        return (UInt24)((uint)left - (uint)right);
+    }
+
+    // Multiplication
+    public static UInt24 operator *(UInt24 left, UInt24 right)
+    {
+        return (UInt24)((uint)left * (uint)right);
+    }
+
+    // Division
+    public static UInt24 operator /(UInt24 left, UInt24 right)
+    {
+        return (UInt24)((uint)left / (uint)right);
+    }
+
+    // Modulus
+    public static UInt24 operator %(UInt24 left, UInt24 right)
+    {
+        return (UInt24)((uint)left % (uint)right);
+    }
+
+    // Increment
+    public static UInt24 operator ++(UInt24 value)
+    {
+        return (UInt24)((uint)value + 1U);
+    }
+
+    // Decrement
+    public static UInt24 operator --(UInt24 value)
+    {
+        return (UInt24)((uint)value - 1U);
+    }
+
+    // Unary Negation
+    public static int operator -(UInt24 value)
+    {
+        return (int)-(uint)value;
+    }
+
+    // Unary Plus
+    public static UInt24 operator +(UInt24 value)
+    {
+        return value;
+    }
+    #endregion
+
+    #region Bitwise Operators
+    // Bitwise AND
+    public static UInt24 operator &(UInt24 left, UInt24 right)
+    {
+        return (UInt24)((uint)left & (uint)right);
+    }
+
+    // Bitwise OR
+    public static UInt24 operator |(UInt24 left, UInt24 right)
+    {
+        return (UInt24)((uint)left | (uint)right);
+    }
+
+    // Bitwise XOR
+    public static UInt24 operator ^(UInt24 left, UInt24 right)
+    {
+        return (UInt24)((uint)left ^ (uint)right);
+    }
+
+    // Left Shift
+    public static UInt24 operator <<(UInt24 value, int shift)
+    {
+        return (UInt24)((uint)value << shift);
+    }
+
+    // Right Shift
+    public static UInt24 operator >>(UInt24 value, int shift)
+    {
+        return (UInt24)((uint)value >> shift);
+    }
+
+    // Ones Complement
+    public static UInt24 operator ~(UInt24 value)
+    {
+        return (UInt24)~(uint)value;
+    }
+    #endregion
+}
